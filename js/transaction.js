47c9cfd730b3837a4c419ab3449c96202bc866d9
tashdid0075 <tashdid75@gmail.com>
2024-09-25 02:39:11 +0600

    added challange part

--- js/transaction.js
+++ js/transaction.js
@@ -1,18 +1,49 @@
 document.getElementById('noakhali-donation-btn').addEventListener('click', function(){
-    const amount = document.getElementById('noakhali-donation-amount').value;
+    const amount = parseFloat(document.getElementById('noakhali-donation-amount').value);
+    if(isNaN(amount) || amount < 0){
+        alert("Failed to make a donation. Try again.");
+        return;
+    }
+    else if(amount > getCurrentBalance()){
+        alert("Not Enough Balance");
+        return;
+    }
+
+
     donate('noakhali-donation', amount);
     addTransaction(amount, "flood victims of Noakhaki");
+    document.getElementById('modal').showModal();
 })
 
 document.getElementById('feni-donation-btn').addEventListener('click', function(){
-    const amount = document.getElementById('feni-donation-amount').value;
+    const amount = parseFloat(document.getElementById('feni-donation-amount').value);
+    if(isNaN(amount || amount < 0)){
+        alert("Failed to make a donation. Try again.");
+        return;
+    }
+    else if(amount > getCurrentBalance()){
+        alert("Not Enough Balance");
+        return;
+    }
+
     donate('feni-donation', amount);
     addTransaction(amount, "flood victims of Feni");
+    document.getElementById('modal').showModal();
 })
 
 document.getElementById('protest-donation-btn').addEventListener('click', function(){
-    const amount = document.getElementById('protest-donation-amount').value;
+    const amount = parseFloat(document.getElementById('protest-donation-amount').value);
+    if(isNaN(amount || amount < 0)){
+        alert("Failed to make a donation. Try again.");
+        return;
+    }
+    else if(amount > getCurrentBalance()){
+        alert("Not Enough Balance");
+        return;
+    }
+
     donate('protest-donation', amount);
     addTransaction(amount, "victims during the protest");
+    document.getElementById('modal').showModal();
 })
 

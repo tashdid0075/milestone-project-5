9e1ce4b1dd7e28c73e33776d10b2b4d0a202c313
tashdid0075 <tashdid75@gmail.com>
2024-09-25 12:29:37 +0600

    all features added

--- js/layout.js
+++ js/layout.js
@@ -2,6 +2,7 @@ document.getElementById('show-donation-btn').addEventListener('click', function(
     showSectionById('donation-section');
     document.getElementById('show-donation-btn').classList.add('bg-lime-400');
     document.getElementById('show-history-btn').classList.remove('bg-lime-400');
+    document.getElementById('footer').classList.remove('hidden');
 
 
 })
@@ -10,6 +11,8 @@ document.getElementById('show-history-btn').addEventListener('click', function()
     showSectionById('history-section');
     document.getElementById('show-history-btn').classList.add('bg-lime-400');
     document.getElementById('show-donation-btn').classList.remove('bg-lime-400');
+    document.getElementById('footer').classList.add('hidden');
 
 
 })
+
